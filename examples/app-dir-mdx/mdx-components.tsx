7242cb3b6945a950528de2918c5d31f5a144e3e2
Wahid Shafique <wahidshafique42@gmail.com>
2023-03-07 16:17:47 -0500

    Update to see if react works in mdx generation

--- examples/app-dir-mdx/mdx-components.tsx
+++ examples/app-dir-mdx/mdx-components.tsx
@@ -4,7 +4,9 @@ import type { MDXComponents } from 'mdx/types'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components, e.g. to add styling.
-    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+    h1: ({ children }) => {
+      return (<h1 onClick={() => { console.log(1} } style={{ fontSize: "100px" }}>{children}</h1>);
+    },
     ...components,
   }
 }
